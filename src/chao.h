c0ef42ac976f93cf7c5f32ca8318bc0e71e59153
rafaelmcuenya <rafael.mcuenya@uel.br>
2025-10-18 17:25:37 -0300

    Update chao.h
    Base do chao.h

--- src/chao.h
+++ src/chao.h
@@ -5,39 +5,40 @@
 #include "forma.h"
 
 /*
-   O chão se trata de um local onde as formas lidas a partir do .geo serão criadas e armazenadas. Será do chão que os carregadores retirarão as formas, e também para onde as formas restantes após a arena retornarão, além do local onde as formas recém criadas, sejam elas pelo .geo ou clonadas, irão ser colocadas.
+   O chão se trata de um local onde as formas lidas a partir do .geo serC#o criadas e armazenadas.
+    Será do chC#o que os carregadores retirarC#o as formas, e também para onde as formas restantes após a arena retornarão, além do local onde as formas recém criadas, sejam elas pelo .geo ou clonadas, irão ser colocadas.
 */
 
 typedef void* Chao;
 
 Chao criaChao();
 /*
-Cria um novo chão vazio.
+Cria um novo chC#o vazio.
 */
 
 void insereFormaChao(Chao c, Forma f);
 /*
-Adiciona uma forma no final do chão.
+Adiciona uma forma no final do chC#o.
 */
 
 Forma retiraFormaChao(Chao c);
 /*
-Remove e retorna a primeira forma do chão.
+Remove e retorna a primeira forma do chC#o.
 */
 
 Forma consultaPrimeiraFormaChao(Chao c);
 /*
-Retorna, sem remover, a primeira forma do chão.
+Retorna, sem remover, a primeira forma do chC#o.
 */
 
 int estaVazioChao(Chao c);
 /*
-Verifica se o chão está vazio.
+Verifica se o chC#o estC! vazio.
 */
 
 void liberaChao(Chao c);
 /*
-Libera toda a memória do chão.
+Libera toda a memC3ria do chC#o.
 */
 
 #endif
