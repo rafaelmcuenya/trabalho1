7c159654fa2ef7a3e4c1f5d1965cc0bc9b1dbd2b
rafaelmcuenya <rafael.mcuenya@uel.br>
2025-10-25 20:41:33 -0300

    Update sobrepor.c

--- src/sobrepor.c
+++ src/sobrepor.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include "sobrepor.h"
+#include "strdupi.h"
 
 static double distancia(double x1, double y1, double x2, double y2){
     return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
