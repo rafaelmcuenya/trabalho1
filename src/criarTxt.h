5cde658c5a163d5c230cf5822b84a5be27e5d463
rafaelmcuenya <rafael.mcuenya@uel.br>
2025-10-25 15:03:14 -0300

    Update criarTxt.h

--- src/criarTxt.h
+++ src/criarTxt.h
@@ -6,13 +6,12 @@
 void iniciarTxt(const char* baseNome);
 void fecharTxt();
 
-void txtLc(int idCarregador, int n);
-void txtShift(Forma forma);
-void txtDsp(Forma forma, double x, double y);
-void txtRjd(int sequencia, Forma forma, double x, double y);
-void txtCalcSobreposicao(Forma f1, Forma f2, int sobreposicao, const char* resultado);
-void txtCalcEsmagada(Forma forma);
-void txtCalcClonada(Forma original, Forma clone);
+void txtLc(int idCarregador, Forma formas[], int count);  
+void txtShift(Forma forma);                              
+void txtDsp(Forma forma, double xFinal, double yFinal);  
+void txtRjd(int sequencia, Forma forma, double xFinal, double yFinal); 
+void txtCalcResultado(const char* resultado);           
+void txtCalcAreaEsmagada(double area);                  
 
 void txtFinal(double pontuacao, int instrucoes, int disparos, int esmagadas, int clonadas);
 
