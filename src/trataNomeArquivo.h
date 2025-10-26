951af438d5cea77212118d07025c7563b93095b6
rafaelmcuenya <rafael.mcuenya@uel.br>
2025-10-26 10:38:44 -0300

    Create trataNomeArquivo.h

---
+++ src/trataNomeArquivo.h
@@ -0,0 +1,41 @@
+#ifndef TRATANOMEARQUIVO_H
+#define TRATANOMEARQUIVO_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+#define PATH_LEN 256
+#define FILE_NAME_LEN 100
+
+/*
+   Módulo responsável por tratar os nomes dos arquivos durante o processamento do programa.
+*/
+
+void extrairNomeBase(const char* caminhoCompleto, char* nomeBase);
+/*
+   Extrai o nome base dos arquivos .geo/.qyr
+*/
+
+void gerarNomeGeoSvg(const char* nomeBase, const char* outputDir, char* caminhoCompleto);
+/*
+   Gera o nome do SVG do arquivo .geo
+*/
+
+void gerarNomeQrySvg(const char* nomeBase, const char* outputDir, char* caminhoCompleto);
+/*
+   Gera o nome do SVG do arquivo .qry, se necessário.
+*/
+
+void gerarNomeTxt(const char* nomeBase, const char* outputDir, char* caminhoCompleto);
+/*
+   Gera o nome do arquivo txt utilizado para os relatórios.
+*/
+
+int criarDiretorioSeNecessario(const char* caminho);
+/*
+   Função extra e de emergência caso seja necessário criar um diretório.
+*/
+
+#endif
